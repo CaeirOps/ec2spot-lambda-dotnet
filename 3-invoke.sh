8c7737fd35137d3c1d1ba7f2b3b7b472e01de679
Ricardo Caeiro <riccaeiro@hotmail.com>
2021-08-24 16:47:44 -0300

    change invoke script

--- 3-invoke.sh
+++ 3-invoke.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -eo pipefail
-FUNCTION=$(aws cloudformation describe-stack-resource --stack-name ec2-spot --logical-resource-id function --query 'StackResourceDetail.PhysicalResourceId' --output text)
+#FUNCTION=$(aws cloudformation describe-stack-resource --stack-name ec2-spot --logical-resource-id function --query 'StackResourceDetail.PhysicalResourceId' --output text)
 
 COUNTER=0
 while [  $COUNTER -lt 10 ]; do
-  aws lambda invoke --function-name $FUNCTION --payload '{"key": "value"}' out.json
+  aws lambda invoke --function-name lambda-dev-test --region us-east-1 --payload '{"key": "value"}' out.json
   cat out.json
   echo ""
   sleep 2
